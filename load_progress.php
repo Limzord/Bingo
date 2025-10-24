3ef61d5e767e71996fa37e1c02a1ef1b3a90bda9
Alissa Maisner <alissa@tmmd.club>
2025-10-24 19:41:46 +0200

    implement functions from file_operations.php into load_progress.php

--- load_progress.php
+++ load_progress.php
@@ -6,22 +6,13 @@ require 'file_operations.php';
 
 $filename = getFileName($_SESSION, session_id());
 
-$dir = __DIR__ . "/saves";
+$data = loadFile($filename);
 
-if (isset($_SESSION['unix_user'])) {
-    $unixUser = $_SESSION['unix_user'];
-    $filename = "$dir/{$unixUser}.json";
-} else {
-    $sessionId = session_id();
-    $filename = "$dir/guest_{$sessionId}.json";
-}
-
-if (!file_exists($filename)) {
+if (!$data) {
     echo json_encode(["success" => false, "message" => "No save found"]);
     exit;
 }
 
-$data = file_get_contents($filename);
 echo json_encode(["success" => true, "data" => json_decode($data, true)]);
 
 ?>
