5e8dae790006d0e713555670f6b4f6a4c7fb1ecc
Alissa Maisner <alissa@tmmd.club>
2025-10-26 16:34:13 +0100

    fix sessions deleting from the server after a day if session lifetime wasn't explicitly set to a different amount in php settings

--- session_manager.php
+++ session_manager.php
@@ -1,5 +1,7 @@
 <?php
 $session_lifetime = 7 * 24 * 60 * 60;
+ini_set('session.gc_maxlifetime', $session_lifetime);
+ini_set('session.cookie_lifetime', $session_lifetime);
 session_name('PHPSESSID');
 session_set_cookie_params([
     'lifetime' => $session_lifetime,
@@ -25,6 +27,9 @@ if (isset($_COOKIE[session_name()])) {
         ]
     );
 }
+
+session_write_close();
+session_start();
 
 header('Content-Type: application/json; charset=utf-8');
 ?>
